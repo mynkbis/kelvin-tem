d1b9db3738fbfdb3773e599f29296c9b1718c94e
mynkbis <bisht.surya@ymail.com>
2021-03-29 12:55:28 +0530

    Newton Scale added

--- kelvin.js
+++ kelvin.js
@@ -1,5 +1,8 @@
-const kelvin = 293;   // variable storing temp in kelvin 
+const kelvin = 0;   // variable storing temp in kelvin 
 const celsius = kelvin-273;  // conversion of temp into celsius
 let Fahrenheit = celsius * (9/5) + 32; //conversion of temp into Fahrenheit
 Fahrenheit = Math.floor(Fahrenheit);
 console.log(`The temperature is ${celsius} C and ${Fahrenheit} F.`);
+let Newton = celsius * (33/100);
+Newton = Math.floor(Newton);
+console.log(`Temp in Newton is ${Newton}`)
